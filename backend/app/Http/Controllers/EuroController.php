3050f7b4cc6c1dd6ced2613296f51bb2e9fc9b02
SzokolAttila <szokol.ati2005@gmail.com>
2024-11-18 12:15:00 +0000

    finished task

--- backend/app/Http/Controllers/EuroController.php
+++ backend/app/Http/Controllers/EuroController.php
@@ -2,16 +2,57 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Exceptions\UnitNotConfiguredException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class EuroController extends Controller
 {
     public function index(){
         $builder = DB::table("euro2024");
+        $items = $builder->orderBy("nation")->paginate(6);
+        $final = DB::table("euro2024")->orderByDesc("played")->take(2)->get();
         return view("euro2024.index", [
             "title" => "Fooldal", 
-            "items" => ,
-            "final" => 
+            "items" => $items,
+            "final" => $final
+        ]);
+    }
+
+    public function hungary(){
+        $builder = DB::table("euro2024");
+        return view("euro2024.hungary", [
+            "title" => "Magyarorszag",
+            "hungary" => $builder->where("nation", "Magyarország")->get()
+        ]);
+    }
+
+    public function nations(){
+        $contains = DB::table("euro2024")->where("nation", "like", "%ország")->orderBy("nation")->get();
+        $notContains = DB::table("euro2024")->where("nation", "not like", "%ország")->orderBy("nation")->get();
+        return view("euro2024.nations", [
+            "title" => "Orszagok", 
+            "contains" => $contains,
+            "notContains" =>  $notContains
+        ]);
+    }
+
+    public function groups($group){
+        $teams = DB::table("euro2024")->where("group", $group)->select("nation", "won", "lost", "drawn")->get();
+        return view("euro2024.groups", [
+            "title" => "Csoport " . $group,
+            "teams" => $teams
+        ]);
+    }
+
+    public function statistics(){
+        return view("euro2024.statistics", [
+            "title" => "Statisztikak", 
+            "avgGoals" => DB::table("euro2024")->avg("goals"),
+            "minGoals" => DB::table("euro2024")->orderBy("goals")->take(1)->select("nation", "goals")->get(),
+            "mostGoalsF" => DB::table("euro2024")->where("group", "F")->orderByDesc("goals")->take(1)->select("nation")->get(),
+            "wonThree" => DB::table("euro2024")->where("won", ">=", 3)->count(),
+            "drawnCount" => DB::table("euro2024")->where("drawn", ">=", 1)->count()
         ]);
     }
 }
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\EuroController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [EuroController::class, "index"])->name("home");
+Route::get('/hungary', [EuroController::class, "hungary"])->name("euro2024.hungary");
+Route::get('/statistics', [EuroController::class, "statistics"])->name("euro2024.statistics");
+Route::get('/nations', [EuroController::class, "nations"])->name("euro2024.nations");
+Route::get('/groups/{group}', [EuroController::class, "groups"])->name("euro2024.groups")->where(["group" => "[ABCDEF]{1}"]);
